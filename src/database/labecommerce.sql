9a7179b5ebcfb4c05c4126c9164621c173a1afd9
Aline Benjamim <alineoak9@gmail.com>
2023-01-19 18:51:02 -0300

    Exercicio Finalizado

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -6,8 +6,6 @@ CREATE TABLE users(
     password TEXT NOT NULL 
 );
 
-SELECT * FROM users;
-
 INSERT INTO users(id, email, password)
 VALUES ("u001", "filipe@labenu.com", "filipe123"),
        ("u002", "barbara@labenu.com", "barbara123"),
@@ -27,4 +25,58 @@ VALUES ("p001", "Vestido", 50, "Roupas e calçados"),
        ("p004", "Bota", 150, "Roupas e calçados"),
        ("p005", "Calça Jeans", 95, "Roupas e calçados");  
 
+
+--Get all users (pegar todos usuarios)
+SELECT * FROM users;
+
+-- Get all Products (pegar todos produtos)
 SELECT * FROM products;
+
+--Search Product by name (selecione o produto por nome)
+SELECT * FROM products
+WHERE name LIKE "%Vestido%";
+
+--Create User (Criar usuario)
+INSERT INTO users(id, email, password)
+VALUES ("u004","maria@labenu.com", "maria123");
+
+--Create Product (criar produto)
+INSERT INTO products(id, name, price, category)
+VALUES ("p006","Luva", 70,"Roupas e calçados");
+
+--Get Products by id (pegar produtos por id)
+SELECT * FROM products
+WHERE id = "p001"; 
+
+--Delete User by id (apague um usuario por id)
+DELETE FROM users
+WHERE id = "u001";
+
+--Delete Product by id (apague um produto por id)
+DELETE FROM products
+WHERE id = "p001";
+
+INSERT INTO products(id, name, price, category) 
+VALUES ("p001", "Vestido", 50, "Roupas e calçados");
+
+--Edit User by id  (editar usuario para id)
+UPDATE users
+SET password = "Maria1234"
+WHERE id = "u004";
+
+--Edit Product by id (editar produto por id)
+UPDATE products
+SET price = 95
+WHERE id = "p001";
+
+--REFATORADO - Get All Users (pegar todos usuarios)
+SELECT * FROM users ORDER BY email ASC;
+
+--Get All Products versão 1 (pegar produtos)
+SELECT * FROM products ORDER BY price ASC
+LIMIT 20 OFFSET 1;
+
+-- Get All Products versão 2 (pegar todos os produtos)
+SELECT * FROM products
+WHERE price >= 100 AND price <= 300
+ORDER BY price ASC;
